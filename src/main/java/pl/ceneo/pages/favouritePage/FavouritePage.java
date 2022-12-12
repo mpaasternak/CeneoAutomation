d02033782720072a330e6c7b2a837cff38dbec74
mpaasternak <mpaasternak@gmail.com>
2022-12-12 15:55:56 +0100

    Created favouirtePage class, located all elements on panel and started creating webelements for it

--- src/main/java/pl/ceneo/pages/favouritePage/FavouritePage.java
+++ src/main/java/pl/ceneo/pages/favouritePage/FavouritePage.java
@@ -1,2 +1,27 @@
-package pl.ceneo.pages.favouritePage;public class FavouritePage {
+package pl.ceneo.pages.favouritePage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import pl.ceneo.utils.BasePage;
+
+public class FavouritePage extends BasePage {
+    public FavouritePage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(className = "asd")
+    WebElement changeFavouriteList;
+
+    @FindBy(className = "asd")
+    WebElement saveFavouriteList;
+
+    @FindBy(className = "asd")
+    WebElement compareProductsFromFavouriteList;
+
+    @FindBy(className = "asd")
+    WebElement checkBundlePriceOfFavouriteList;
+
+    @FindBy(className = "asd")
+    WebElement showAllFavouriteLists;
 }
